399797c50fa99bfd770d6cb8c251afe395805ba7
何旭東 <>
2019-10-17 15:45:38 +0800

    函数 闭包 使用

--- 2-1/interface/interface_main.go
+++ 2-1/interface/interface_main.go
@@ -2,27 +2,26 @@ package main
 
 import "fmt"
 
-type Iname interface {
-	Mname()
+//  https://juejin.im/post/5c9d5631f265da60d82dde9c
+type Shape interface {
+	Area() float32
 }
-type St struct{}
 
-func (St) Mname() {}
+type Rect struct {
+	width  float32
+	height float32
+}
+
+func (r Rect) Area() float32 {
+	return r.width * r.height
+}
+
 func main() {
-	var t *St
-
-	fmt.Printf("t type is %T\n", t)
-	if t == nil {
-		fmt.Println("t is nil")
-	} else {
-		fmt.Println("t is not nil")
-	}
-	var i Iname = t
-	fmt.Printf("%T\n", i)
-	if i == nil {
-		fmt.Println("i is nil")
-	} else {
-		fmt.Println("i is not nil")
-	}
-	fmt.Printf("i is nil pointer:%v", i == (*St)(nil))
+	var s Shape
+	s = Rect{5.0, 4.0}
+	//r := Rect{5.0, 4.0}
+	//fmt.Printf("type of s is %T\n", s)
+	//fmt.Printf("value of s is %v\n", s)
+	fmt.Println("area of rectange s", s.Area())
+	//fmt.Println("s == r is", s == r)
 }
